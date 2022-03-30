342f4893e153fe2a0407200425b5b707d8d4cdc3
unknown <jtpaquet@gmail.com>
2022-03-30 12:50:59 -0400

    n mot compteur :(

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ t0 = datetime.now()
 connection = MongoClient(MONGODB_URI)
 database = connection[DBS_NAME]
 members = database['members']
-messages = database['messages_8dec2021']
+messages = database['messages_13sept2021']
 print('connexion time:', datetime.now()-t0)
 pseudos = {author['name'] : author['pseudo'] for author in list(members.find())}
 connection.close()
@@ -44,50 +44,87 @@ app.title = f'Touka Analytics - {choice(noms_tabs)}'
 
 
 # Compile overall data on whole database
+
 t0 = datetime.now()
 data = {}
+
 data['n_msg'] = {pseudos[d['_id']] : d['count'] for d in list(messages.aggregate([{"$sortByCount": "$author"}]))}
+
 n_word_pipeline = [{"$match": {"content": {"$exists":True}}},{"$project": {"author": 1, "n_word": {"$size": {"$split": ["$content", " "]}}}}, {"$group" : { "_id" : "$author", "n_word" : {"$sum":"$n_word"}}}]
+
 data['n_word'] = {pseudos[d['_id']] : d['n_word'] for d in list(messages.aggregate(n_word_pipeline))}
+
 n_char_pipeline = [{"$match": {"content": {"$exists":True}}},{"$project": {"author": 1, "n_char": {"$strLenCP" : "$content"}}}, {"$group" : { "_id" : "$author", "n_char" : {"$sum":"$n_char"}}}]
+
 data['n_char'] = {pseudos[d['_id']] : d['n_char'] for d in list(messages.aggregate(n_char_pipeline))}
+
 data['ratio_char_msg'] = {name : data['n_char'][name]/data['n_msg'][name] for name in pseudos.values()}
+
 data['ratio_word_msg'] = {name : data['n_word'][name]/data['n_msg'][name] for name in pseudos.values()}
+
 data['total_msg'] = list(messages.aggregate( [ { "$collStats": { "storageStats": { } } } ] ))[0]['storageStats']['count']
+
 data['date_min'] = list(messages.aggregate([{"$group":{"_id": {}, "date_min": { "$min": "$timestamp" }}}]))[0]['date_min']
+
 data['date_max'] = list(messages.aggregate([{"$group":{"_id": {}, "date_max": { "$max": "$timestamp" }}}]))[0]['date_max']
+
 msg_by_month_pipeline = [{"$project": {"date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : { "$dateToString": { "format": "%m-%Y", "date": "$date" }}, "n_msg": {"$sum": 1}}}]
+
 msg_by_year_pipeline = [{"$project": {"date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : { "$dateToString": { "format": "%Y", "date": "$date" }}, "n_msg": {"$sum": 1}}}]
+
 msg_by_author_by_hour_pipeline = [{"$project": {"author": 1, "date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : {"author": "$author", "hour": {"$dateToString": { "format": "%H", "date": "$date" }}}, "n_msg": {"$sum": 1}}}]
+
 msg_by_author_by_weekday_pipeline = pipeline = [{"$project": {"author": 1, "date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : {"author": "$author", "weekday": {"$dateToString": { "format": "%w", "date": "$date" }}}, "n_msg": {"$sum": 1}}}]
+
 msg_by_author_by_month_pipeline = [{"$project": {"author": 1, "date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : {"author": "$author", "date": {"$dateToString": { "format": "%m-%Y", "date": "$date" }}}, "n_msg": {"$sum": 1}}}]
-msg_by_author_by_year_pipeline = pipeline = [{"$project": {"author": 1, "date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : {"author": "$author", "year": {"$dateToString": { "format": "%Y", "date": "$date" }}}, "n_msg": {"$sum": 1}}}]
+
+msg_by_author_by_year_pipeline = [{"$project": {"author": 1, "date" : {"$toDate" : "$timestamp"}}}, {"$group" : {"_id" : {"author": "$author", "year": {"$dateToString": { "format": "%Y", "date": "$date" }}}, "n_msg": {"$sum": 1}}}]
+
+n_mot_counter_pipeline = [{"$match": {"content": {"$regex": 'nigg', '$options' : 'i'}}}, {"$project": {"author": 1, "n_mot": { "$sum": 1}}}, {"$group": {"_id": "$author", "n_mot": { "$sum": '$n_mot'}}}, {"$sort": {"n_mot": -1}}]
+
 # Faire les groupes par hour, weekday et month
+
 # data['n_msg_by_hour'] = {(d['_id']['author'], d['_id']['hour'] ) : d['n_msg'] for d in messages.aggregate(msg_by_author_by_hour_pipeline)}
+
 # data['n_msg_by_weekday'] = {(d['_id']['author'], d['_id']['weekday'] ) : d['n_msg'] for d in messages.aggregate(msg_by_author_by_weekday_pipeline)}
+
 # data['n_msg_by_month'] = {(d['_id']['author'], d['_id']['date'] ) : d['n_msg'] for d in messages.aggregate(msg_by_author_by_month_pipeline)}
+
 # data['n_msg_by_year'] = {(d['_id']['author'], d['_id']['year'] ) :  d['n_msg'] for d in messages.aggregate(msg_by_author_by_year_pipeline)}
+
 # data['total_msg_by_month'] = {d['_id']: d['n_msg'] for d in messages.aggregate(msg_by_month_pipeline)}
+
 # data['total_msg_by_year'] = {d['_id']: d['n_msg'] for d in messages.aggregate(msg_by_year_pipeline)}
+
 data['n_msg_by_hour'] = list(messages.aggregate(msg_by_author_by_hour_pipeline))
 data['n_msg_by_weekday'] = list(messages.aggregate(msg_by_author_by_weekday_pipeline))
 data['n_msg_by_month'] = list(messages.aggregate(msg_by_author_by_month_pipeline))
 data['n_msg_by_year'] = list(messages.aggregate(msg_by_author_by_year_pipeline))
 data['total_msg_by_month'] = list(messages.aggregate(msg_by_month_pipeline))
 data['total_msg_by_year'] = list(messages.aggregate(msg_by_year_pipeline))
+data['n_mot_counter'] = list(messages.aggregate(n_mot_counter_pipeline))
+
 # Reactions
+
 # react_received_by_author_pipeline = [{ "$group": {"_id": "$author", "count": {"$sum":  {"$size": "$reactions"}}} }]
+
 # data['react_received_by_author'] = list(messages.aggregate(react_received_by_author_pipeline))
+
 react_received_by_author_and_type_pipeline = [{"$unwind": "$reactions"}, {"$group": {"_id": {"author":"$author", "reaction": "$reactions.reaction" }, "count": {"$sum":1}}}]
+
 data['react_received_by_author_and_type'] = list(messages.aggregate(react_received_by_author_and_type_pipeline))
 react_made_by_actor_pipeline = [{"$unwind": "$reactions"}, {"$sortByCount": "$reactions.actor"}]
+
 data['react_made_by_actor'] = list(messages.aggregate(react_made_by_actor_pipeline))
 react_made_by_actor_and_reaction_pipeline = [{"$unwind": "$reactions"}, {"$group": {"_id": {"actor":"$reactions.actor", "reaction": "$reactions.reaction" }, "count": {"$sum":1}}}]
+
 data['react_made_by_actor_and_reaction'] = list(messages.aggregate(react_made_by_actor_and_reaction_pipeline))
 react_made_by_reaction_pipeline = [{"$unwind": "$reactions"}, {"$sortByCount": "$reactions.reaction"}]
+
 data['react_made_by_reaction'] = list(messages.aggregate(react_made_by_reaction_pipeline))
 
-print('compiling data time: ', datetime.now()-t0)
+print('compiling data time: ', datetime.now() - t0)
+
 print("Data compiled")
 
 msg_total = data["total_msg"]
@@ -167,7 +204,6 @@ fig3.update_layout(
     height=500
 )
 
-
 # Bar chart reacts per person
 
 data_ = data["react_made_by_actor"]
@@ -186,6 +222,24 @@ fig4 = px.bar(df_react_per_person, x='_id', y="count", title='Réeactions par to
                      "count": "Réactions"})
 
 fig4.update_layout(
+    autosize=False,
+    height=500
+)
+
+
+# Bar charts n mot counter
+
+data_ = data['n_mot_counter']
+
+for doc in data_:
+    doc["_id"] = pseudos[doc["_id"]]
+
+df_n_mot_counter = pd.DataFrame.from_dict(data_)
+fig5 = px.bar(df_n_mot_counter, x='_id', y="n_mot", title='N mot compteur :(', labels={
+                     "_id": "Touka",
+                     "n_mot": "N mots"})
+
+fig5.update_layout(
     autosize=False,
     height=500
 )
@@ -227,6 +281,11 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='react-per-touka',
         figure=fig4
+    ),
+
+    dcc.Graph(
+        id='n-mot-per-touka',
+        figure=fig5
     )
 ])
 
